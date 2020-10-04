b93014f03caeefe9c6a61053656b95c12d74d5af
KORAblinEC <korablinec@gmail.com>
2020-10-04 20:09:59 +0300

    Выполненная третья домашка

--- src/ru/geekbrains/lesson3/Fourapp.java
+++ src/ru/geekbrains/lesson3/Fourapp.java
@@ -5,19 +5,35 @@ import java.util.Scanner;
 
 public class Fourapp {
     public static void main(String[] args) {
-        String xxx = "xxxxxxxxxxxxxxx";
-        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
-        System.out.println("Компьютер загадал одно из этих слов: ");
-            for(int i = 0; i < words.length; i++){
-                System.out.println(words[i]);
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
+                "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut",
+                "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        //загадаем слово из массива
+        Random random = new Random();
+        int indWord = random.nextInt(words.length - 1);
+        String word = words[indWord];
+        System.out.println("Я загадал одно из этих слов, попробуй отгадать какое");
+        for (int i=0;i<words.length;i++)
+            System.out.println(words[i]);
+        //инициализируем сканер
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Введи свое слово: (для выхода Enter)");
+            String answer = scanner.nextLine();
+            if (answer.equals("")) break;
+            else if (word.equals(answer)) {
+                System.out.println("Вы угадали слово, игра закончена!!!");
+                break;
             }
-            Random random = new Random();
-            int index = random.nextInt(words.length);
-            //System.out.println(words[index]);
-            System.out.println("Попробуйте угадать какое слово он загадал: введите ответ ниже");
-            Scanner sc = new Scanner(System.in);
-            String a = sc.next();
-            String b = words[index];
-
+            char[] charsAnswer = answer.toCharArray();
+            for (int i = 0; i < word.length(); i++) {
+                if (i >= charsAnswer.length) break;
+                if (word.charAt(i) != charsAnswer[i]) charsAnswer[i] = '#';
+            }
+            StringBuilder comment = new StringBuilder(String.valueOf(charsAnswer));
+            for (int i = comment.length(); i < 15; i++) comment.append("#");
+            System.out.println(comment);
+        }
+        while (true);
     }
 }
